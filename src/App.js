cb30ab26bf1943f50c69e84dbeed247200060afe
siddharth17vaishnav <siddharth17vaishnav@gmail.com>
2022-07-01 17:41:24 +0530

    Netflix Delpoyed on github

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Routes,Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
+import Home from './pages/Home';
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import ProtectedRoute from './components/ProtectedRoute.js'
+
+
+
+function App() {
+  return (
+    <>
+    <AuthContextProvider>
+      <Navbar/>
+      <Routes>
+      
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/account' element={<ProtectedRoute><Account/></ProtectedRoute>}/>
+      </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
